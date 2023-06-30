4459c0479c0b3386bd3f19cacdfb6d56c070d82c
tasnim <tasnim0tantawi@gmail.com>
2023-06-30 16:41:32 +0300

    admin configurations

--- ecommerce/urls.py
+++ ecommerce/urls.py
@@ -1,22 +1,16 @@
-"""
-URL configuration for ecommerce project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+# Now we have a unique url path for our images.
+# When we upload an image, it will be saved in the 'media' folder after creating a folder with the name 'images'.
+# static/media/images/imagename.jpg
+# static() function is used to serve static files like images, css, js, etc.
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- store/admin.py
+++ store/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
 
 # Register your models here.
+from . models import Category, Product
+
+
+@admin.register(Category)
+# make the slug field auto-populate from the name field (prepopulated_fields),
+# slug will be the same as the name but in lowercase and with hyphens instead of spaces.
+class CategoryAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('name',)}
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug': ('title',)}
