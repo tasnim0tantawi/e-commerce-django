9b8ef76bcf3f6490b0e07b1ceb0e2dcef1b803b3
tasnim <tasnim0tantawi@gmail.com>
2023-06-30 18:09:56 +0300

    migrations

--- store/models.py
+++ store/models.py
@@ -23,6 +23,10 @@ class Category(models.Model):
 
 
 class Product(models.Model):
+    # FK
+
+    category = models.ForeignKey(Category, related_name='product', on_delete=models.CASCADE, null=True)
+
     title = models.CharField(max_length=250)
     brand = models.CharField(max_length=250, default='un-branded')
     # blank=True means that this field is optional
