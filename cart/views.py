45b01db55f760ddd367656404287f0848b7e1b62
tasnim <tasnim0tantawi@gmail.com>
2023-07-03 02:07:35 +0300

    cart configuration

--- cart/views.py
+++ cart/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
 def cart_summary(request):
-    pass
+    return render(request, 'cart/cart-summary.html')
+
 
 def cart_add(request):
     pass
