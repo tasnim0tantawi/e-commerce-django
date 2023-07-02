64f87237011908832fafc4ad94e34de677ae32d8
tasnim <tasnim0tantawi@gmail.com>
2023-07-02 23:29:12 +0300

    product info

--- store/views.py
+++ store/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Category, Product
 
+from django.shortcuts import get_object_or_404
+
 # Create your views here.
 
 
@@ -22,3 +24,9 @@ def categories(request):
     return context
 
 
+def product_info(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    context = {
+        'product': product,
+    }
+    return render(request, 'store/product_info.html', context=context)
